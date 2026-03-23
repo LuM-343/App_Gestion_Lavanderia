f39326ba1089757bfaa45284c210060fc7b5d385
arenz <marenz.pelaez@gmail.com>
2026-03-22 21:56:24 -0600

    Se agregan métodos en LaundryDao para insertar y consultar movimientos

--- app/src/main/java/com/example/javeslaundry/LaundryDao.kt
+++ app/src/main/java/com/example/javeslaundry/LaundryDao.kt
@@ -8,15 +8,24 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface LaundryDao {
 
+    // CLIENTES
     @Insert
     suspend fun insertarCliente(cliente: Cliente)
 
     @Query("SELECT * FROM clientes ORDER BY id DESC")
     fun obtenerClientes(): Flow<List<Cliente>>
 
+    // LAVADAS
     @Insert
     suspend fun insertarLavada(lavada: Lavada)
 
     @Query("SELECT * FROM lavadas ORDER BY id DESC")
     fun obtenerLavadas(): Flow<List<Lavada>>
+
+    // MOVIMIENTOS
+    @Insert
+    suspend fun insertarMovimiento(movimiento: Movimiento)
+
+    @Query("SELECT * FROM movimientos ORDER BY fecha DESC")
+    fun obtenerMovimientos(): Flow<List<Movimiento>>
 }
