5e77f901842d37aebeb89227362ab0cc21b08a5e
arenz <marenz.pelaez@gmail.com>
2026-03-22 18:49:49 -0600

    Se agrega entidad Cliente para base de datos Room

--- app/src/main/java/com/example/javeslaundry/Cliente.kt
+++ app/src/main/java/com/example/javeslaundry/Cliente.kt
@@ -1,2 +1,13 @@
 package com.example.javeslaundry
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "clientes")
+data class Cliente(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nombre: String,
+    val telefono: String,
+    val direccion: String
+)
