5e86143bb9ee68c0549fcce28d85e5b6cab0ded9
arenz <marenz.pelaez@gmail.com>
2026-03-22 21:58:01 -0600

    Se actualiza AppDatabase para incluir la entidad Movimiento

--- app/src/main/java/com/example/javeslaundry/AppDatabase.kt
+++ app/src/main/java/com/example/javeslaundry/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(
-    entities = [Cliente::class, Lavada::class],
+    entities = [Cliente::class, Lavada::class, Movimiento::class],
     version = 1,
     exportSchema = false
 )
